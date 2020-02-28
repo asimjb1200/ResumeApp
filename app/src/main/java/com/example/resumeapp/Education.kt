1c1b8d9f051ac3a793f4df637a88515e3972e254
Asim <ajbp68@mail.umsl.edu>
2020-02-27 19:03:59 -0600

    setting up education page

---
+++ app/src/main/java/com/example/resumeapp/Education.kt
@@ -0,0 +1,15 @@
+package com.example.resumeapp
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import android.widget.TextView
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class Education: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_education);
+    }
+}
