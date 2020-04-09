85fa26f13f32f325b9bea5e28119fca3b0c575c3
Asim <ajbp68@mail.umsl.edu>
2020-04-08 22:47:49 -0500

    getting the contact me page ready

---
+++ app/src/main/java/com/example/resumeapp/ContactMe.kt
@@ -0,0 +1,11 @@
+package com.example.resumeapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+public class ContactMe: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contactMe)
+    }
+}
